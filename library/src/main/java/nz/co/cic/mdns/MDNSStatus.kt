41384f4783ad8290445d3e14e479eb9781ae6aa0
Bobbybill123 <jacob.cohn.gell@gmail.com>
2017-03-14 16:25:24 +1300

    Added ServiceRegistered and ServiceUnregistered statuses to their respective functions

--- library/src/main/java/nz/co/cic/mdns/MDNSStatus.kt
+++ library/src/main/java/nz/co/cic/mdns/MDNSStatus.kt
@@ -4,4 +4,13 @@ package nz.co.cic.mdns
  * Created by dipshit on 9/03/17.
  */
 
-class MDNSStatus
+class MDNSStatus (status : String) {
+    private var status:String? = null
+    init {
+        this.status = status
+    }
+
+    fun getStatus() : String? {
+        return status
+    }
+}
