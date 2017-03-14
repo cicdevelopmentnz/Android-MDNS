aef7b1801e4b708c45cd705dd7a32fdc9fb85b5b
Bobbybill123 <jacob.cohn.gell@gmail.com>
2017-03-15 10:05:34 +1300

    Removed a variable statement from MDNSStatus as it was unnecessary

--- library/src/main/java/nz/co/cic/mdns/MDNSStatus.kt
+++ library/src/main/java/nz/co/cic/mdns/MDNSStatus.kt
@@ -4,12 +4,7 @@ package nz.co.cic.mdns
  * Created by dipshit on 9/03/17.
  */
 
-class MDNSStatus (status : String) {
-    private var status:String? = null
-    init {
-        this.status = status
-    }
-
+class MDNSStatus (private val status : String) {
     fun getStatus() : String? {
         return status
     }
